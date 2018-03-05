4ac8593ef209cd90f9ccd50a9f10427f43456e69
Vincent <const.lomo@gmail.com>
2018-03-05 11:39:03 +0800

    reafctor

--- src/createContext.js
+++ src/createContext.js
@@ -1,19 +1,49 @@
-import React, {createContext} from "react";
+import React from "react";
 
-module.exports = function(defaultValue, calculateChangedBits){
-  const Context = createContext(defaultValue, calculateChangedBits);
-  class Provider extends React.Component {
-    state = defaultValue;
-    __context = {setState: this::this.setState, forceUpdate: this::this.setState};
-    render() {
+module.exports = function createContext(Context){
+  const {Provider, Consumer} = React.createContext();
+
+  if(!Context.prototype.setState) {
+    Context.prototype.setState = function(partialState, callback) {
+      this.__element.setState(partialState, callback);
+    }
+  }
+  if(!Context.prototype.forceUpdate) {
+    Context.prototype.forceUpdate = function (callback) {
+      this.__element.forceUpdate(callback);
+    }
+  }
+
+  class ContextProvider extends React.Component {
+    constructor(props){
+      super();
+
+      const context = new Context(props);
+      const defaultState = context.state || {};
+      context.__element = this;
+      Object.defineProperty(context, 'state', {
+        get: function(){
+          return this.__element.state;
+        }
+      });
+
+      this.state = defaultState;
+
+      this.__context = context;
+      this.__contextCopy = Object.create(context);
+      this.__value = null;
+    }
+    render(){
+      this.__value = this.__value==this.__context?this.__contextCopy:this.__context; // value forceUpdate
       return (
-        <Context.Provider value={{...this.__context, state: this.state}}>
-          {this.props.children}
-        </Context.Provider>
+        <Provider value={ this.__value }>
+          { this.props.children }
+        </Provider>
       );
     }
   }
-  Provider.Provider = Provider;
-  Provider.Consumer = Context.Consumer;
-  return Provider;
-}
+
+  Consumer.Provider = ContextProvider;
+  ContextProvider.context = Consumer;
+  return Consumer;
+};
